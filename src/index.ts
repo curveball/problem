5bc689725ed2c3f77553e90ff4e1d8dfd245cf4d
Evert Pot <me@evertpot.com>
2019-05-04 01:18:08 -0400

    Fully working middleware

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,84 @@
-/* tslint:disable */
-console.log('Hello world!');
+import { Middleware } from '@curveball/core';
+import { HttpProblem, isClientError, isHttpError } from '@curveball/http-errors';
+
+type ProblemMwSettings = {
+  debug: boolean,
+};
+
+export default function(settings?: ProblemMwSettings): Middleware {
+
+  let debugMode = false;
+  if (settings && settings.debug !== undefined) {
+    debugMode = settings.debug;
+  } else if (process.env.NODE_ENV === 'development') {
+    debugMode = true;
+  }
+
+  return async (ctx, next) => {
+
+    try {
+      await next();
+    } catch (e) {
+
+      let status: number;
+      let clientError = false;
+      let title = e.message;
+      let detail;
+
+      if (isHttpError(e)) {
+        status = e.httpStatus;
+        clientError = isClientError(e);
+        if ((<HttpProblem> e).title) {
+          title = (<HttpProblem> e).title;
+        }
+        if ((<HttpProblem> e).detail) {
+          detail = (<HttpProblem> e).detail;
+        }
+
+      } else {
+        status = 500;
+        if (!debugMode) {
+          title = 'Internal Server Error';
+        }
+      }
+
+      ctx.response.status = status;
+      ctx.response.headers.set('Content-Type', 'application/problem+json');
+
+      // 401 errors
+      if (e.wwwAuthenticate) {
+        ctx.response.headers.set('WWW-Authenticate', e.wwwAuthenticate);
+      }
+
+      // 405 errors
+      if (e.allow) {
+        ctx.response.headers.set('Allow', e.allow.join(', '));
+      }
+
+      // 413, 429 or 504 errors
+      if (e.retryAfter) {
+        ctx.response.headers.set('Retry-After', e.retryAfter);
+      }
+
+      ctx.response.body = {
+        title,
+        status
+      };
+
+      if (detail) {
+        ctx.response.body.detail = detail;
+      }
+
+      if (clientError) {
+        // tslint:disable-next-line no-console
+        console.warn(e);
+      } else {
+        // tslint:disable-next-line no-console
+        console.error(e);
+      }
+
+    }
+
+  };
+
+}
---
+++ test/index.ts
@@ -0,0 +1,147 @@
+import { Application, Middleware } from '@curveball/core';
+import problemMw from '../src/index';
+import { expect } from 'chai';
+import { NotFound, ServiceUnavailable, MethodNotAllowed, Unauthorized } from '@curveball/http-errors';
+
+class BlandError extends Error {
+  httpStatus = 420
+};
+
+const tester = (fun: Middleware, debug?: boolean) => {
+
+  const settings = debug !== undefined ? { debug } : undefined;
+
+  const app = new Application();
+  app.use( problemMw(settings) );
+  app.use( fun );
+
+  return app.subRequest('GET', '/');
+
+};
+
+describe('Problem middleware', () => {
+
+  it('should convert errors', async () => {
+
+    const res = await tester( () => {
+      throw new Error('Hello');
+    });
+
+    expect(res.status).to.equal(500);
+    expect(res.body).to.eql({
+      title: 'Internal Server Error',
+      status: 500,
+    });
+
+  });
+
+  it('should handle errors from the http-errors package', async () => {
+
+    const res = await tester( () => {
+      throw new NotFound('Hello');
+    });
+
+    expect(res.status).to.equal(404);
+    expect(res.body).to.eql({
+      title: 'Not Found',
+      status: 404,
+      detail: 'Hello',
+    });
+
+  });
+
+  it('should generate a Retry-After header for exceptions that support it', async () => {
+
+    const res = await tester( () => {
+      throw new ServiceUnavailable('FooBar', 5);
+    });
+
+    expect(res.status).to.equal(503);
+    expect(res.body).to.eql({
+      title: 'Service Unavailable',
+      status: 503,
+      detail: 'FooBar',
+    });
+    expect(res.headers.get('Retry-After')).to.eql('5');
+
+  });
+
+  it('should generate an Allow header for 405 errors', async () => {
+
+    const res = await tester( () => {
+      throw new MethodNotAllowed('Zzz', ['PUT', 'PATCH']);
+    });
+
+    expect(res.status).to.equal(405);
+    expect(res.body).to.eql({
+      title: 'Method Not Allowed',
+      status: 405,
+      detail: 'Zzz',
+    });
+    expect(res.headers.get('Allow')).to.eql('PUT, PATCH');
+
+  });
+
+  it('should set a WWW-Authenticate header for 401 errors', async () => {
+
+    const res = await tester( () => {
+      throw new Unauthorized('Zzz', ['Bearer', 'Digest']);
+    })
+
+    expect(res.status).to.equal(401);
+    expect(res.body).to.eql({
+      title: 'Unauthorized',
+      status: 401,
+      detail: 'Zzz',
+    });
+    expect(res.headers.get('WWW-Authenticate')).to.eql('Bearer, Digest');
+
+  });
+
+  it('should send debug messages when debugMode is on via a flag', async () => {
+
+    const res = await tester( () => {
+      throw new Error('Hello');
+    }, true);
+
+    expect(res.status).to.equal(500);
+    expect(res.body).to.eql({
+      title: 'Hello',
+      status: 500,
+    });
+
+  });
+
+  it('should also turn on debug mode if an environment flag is set', async () => {
+
+    process.env.NODE_ENV = 'development';
+
+    const res = await tester( () => {
+      throw new Error('Hello');
+    });
+
+    expect(res.status).to.equal(500);
+    expect(res.body).to.eql({
+      title: 'Hello',
+      status: 500,
+    });
+
+    delete process.env.NODE_ENV;
+
+  });
+
+  it('should also correctly pick up "httpError" properties', async () => {
+
+    const res = await tester( () => {
+      throw new BlandError('Hello');
+    });
+
+    expect(res.status).to.equal(420);
+    expect(res.body).to.eql({
+      title: 'Hello',
+      status: 420,
+    });
+
+  });
+
+});
