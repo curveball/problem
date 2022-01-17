139e5d596937f25f18ae05e8ece025c2bbfd15f8
Evert Pot <me@evertpot.com>
2022-01-17 03:39:46 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Middleware } from '@curveball/core';
 import { isClientError, isHttpError, isHttpProblem } from '@curveball/http-errors';
 
 type ProblemMwSettings = {
-  debug?: boolean,
-  quiet?: boolean,
+  debug?: boolean;
+  quiet?: boolean;
 };
 
 export default function(settings?: ProblemMwSettings): Middleware {
